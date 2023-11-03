26589ab26047d69436cbbb30a2cbd3b018befb9f
Oscar Wendt <oscar.wendt91@gmail.com>
2023-11-03 19:41:21 +0100

    added login connection to backend, updated credentials to username instead of email

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const userData = {
+      username,
+      password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/api/identity/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("login successful", data);
+      } else {
+        console.error("login failed");
+      }
+    } catch (error) {
+      console.error(error, "Something went wrong");
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleLogin}>
       <label>
         Username:
-        <input type="text" />
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </label>
       <label>
         Password:
-        <input type="password" />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
       <button type="submit">Login</button>
     </form>
