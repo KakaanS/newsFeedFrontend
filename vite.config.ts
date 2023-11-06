df2c4cab1eea7553f97e03c203f6f499cdedf314
Oscar Wendt <oscar.wendt91@gmail.com>
2023-11-06 13:26:28 +0100

    adding test to pass

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [react()],
   base: "/",
 });
