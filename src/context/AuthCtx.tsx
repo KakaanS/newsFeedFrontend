bbe3683ee5d2a0ac39466fc01f37a1d271abb0fc
nordinsimon <simon.s.nordin@gmail.com>
2023-11-07 22:50:14 +0100

    Add Axios And intercept

--- src/context/AuthCtx.tsx
+++ src/context/AuthCtx.tsx
@@ -1,7 +1,8 @@
 import { createContext, useContext, useEffect, ReactNode } from "react";
 import { useCookies } from "react-cookie";
 import { useLocation, useNavigate } from "react-router-dom";
-
+import axios, { AxiosError } from "axios";
+import api from "../middleware/api";
 export interface LoginData {
   accessToken: string;
   refreshToken: string;
@@ -39,60 +40,66 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     navigate("/login");
   };
 
-  const handleRefreshToken = () => {
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response.status === 401) {
+        const responseData = error.response.data;
+        if (responseData.error === "Token not authorized") {
+          handleRefreshToken();
+        }
+      }
+      return Promise.reject(error);
+    },
+  );
+
+  const handleRefreshToken = async () => {
     if (refreshToken !== null) {
-      fetch("http://localhost:3000/api/identity/refresh", {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + refreshToken,
-        },
-      })
-        .then((response) => {
-          console.log("response", response);
-          if (response.ok) {
-            return response.json();
-          } else {
-            logout();
-            throw new Error("Failed to refresh token");
-          }
-        })
-        .then((data) => {
-          console.log("data", data);
-          setCookie("accessToken", data.accessToken);
-        })
-        .catch((error) => {
-          console.error("Token refresh failed: ", error);
-          logout();
+      try {
+        const response = await api.get("/identity/refresh", {
+          headers: {
+            Authorization: "Bearer " + refreshToken,
+          },
         });
+
+        if (response.status === 200) {
+          setCookie("accessToken", response.data.accessToken);
+        } else {
+          logout();
+          throw new Error("Failed to refresh token");
+        }
+      } catch (error) {
+        console.error("Token refresh failed: ", error);
+        logout();
+      }
     }
   };
 
-  //USE EFFECT TO CHECK IF TOKEN IS VALID
   useEffect(() => {
-    console.log("USE EFFECT TO CHECK IF TOKEN IS VALID");
-
-    const notAuthenticated = () => {
-      fetch("http://localhost:3000/api/identity/verifyToken", {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + accessToken,
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            return true;
-          } else if (response.status === 401) {
-            removeCookie("accessToken");
-            handleRefreshToken();
-          } else {
-            logout();
-            throw new Error("Token is not valid");
-          }
-        })
-        .catch((error) => {
-          console.error("Something went wrong", error);
-          logout();
+    const notAuthenticated = async () => {
+      try {
+        const response = await api.get("/identity/verifyToken", {
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
         });
+
+        if (response.status === 200) {
+          console.log("Token is valid");
+          return true;
+        } else if (response.status === 401) {
+          removeCookie("accessToken");
+          handleRefreshToken();
+        } else {
+          logout();
+          throw new Error("Token is not valid");
+        }
+      } catch (error) {
+        if ((error as AxiosError)?.response?.status === 401) return;
+        console.error(error);
+      }
     };
 
     notAuthenticated();
@@ -101,11 +108,12 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     if (refreshToken === null) {
+      removeCookie("accessToken");
       if (location.pathname === "/register") return;
       navigate("/login");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [navigate, location.pathname]);
+  }, [navigate, location.pathname, refreshToken]);
 
   const value = {
     accessToken,
