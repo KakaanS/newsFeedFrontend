e851c10646f54ea7bb8cabc750b1e59dd9625547
Oscar Wendt <oscar.wendt91@gmail.com>
2023-11-08 22:01:12 +0100

    trying to solve CSS

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 //Content
 import "./Navbar.css";
 import { useAuth } from "../../context/AuthCtx";
+import { useLocationContext } from "../../context/LocationCtx";
 
 interface NavbarProps {
   logout: () => void;
@@ -13,6 +14,8 @@ const Navbar: React.FC<NavbarProps> = ({ logout }) => {
   const navigate = useNavigate();
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { role } = useAuth() as any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { adminView, currentView } = useLocationContext() as any;
 
   const navigateHome = () => {
     navigate("/");
@@ -27,16 +30,31 @@ const Navbar: React.FC<NavbarProps> = ({ logout }) => {
 
   return (
     <div className="sidebar">
-      <button onClick={navigateHome} className="sidebarBtn">
+      <button
+        onClick={navigateHome}
+        className={`sidebarBtn ${
+          currentView === "Home" ? "active-button" : ""
+        }`}
+      >
         Home
       </button>
       {role === "admin" && (
-        <button onClick={navigateAdmin} className="sidebarBtn">
+        <button
+          onClick={navigateAdmin}
+          className={`sidebarBtn ${
+            currentView === "Admin Panel" ? "active-button" : ""
+          }`}
+        >
           Admin Panel
         </button>
       )}
       {role === "admin" && (
-        <button onClick={navigateAddNewArticle} className="sidebarBtn">
+        <button
+          onClick={navigateAddNewArticle}
+          className={`sidebarBtn ${
+            currentView === "Add New Article" ? "active-button" : ""
+          }`}
+        >
           Add new article
         </button>
       )}
--- src/context/LocationCtx.tsx
+++ src/context/LocationCtx.tsx
@@ -1,9 +1,16 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
 
 const LocationContext = createContext({
   adminView: "Admin Panel",
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   updateAdminView: (_newView: string) => {},
+  currentView: "/",
 });
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -16,13 +23,29 @@ interface LocationProviderProps {
 
 export function LocationProvider({ children }: LocationProviderProps) {
   const [adminView, setAdminView] = useState("Admin Panel");
+  const [currentView, setCurrentView] = useState("adminView");
 
   const updateAdminView = (newView: string) => {
     setAdminView(newView);
+    setCurrentView(newView);
+  };
+
+  useEffect(() => {
+    const initialLocation = window.location.pathname;
+    if (initialLocation === "/") {
+      setCurrentView("Home");
+    }
+    setCurrentView(adminView);
+  }, [adminView]);
+
+  const values = {
+    adminView,
+    updateAdminView,
+    currentView,
   };
 
   return (
-    <LocationContext.Provider value={{ adminView, updateAdminView }}>
+    <LocationContext.Provider value={values}>
       {children}
     </LocationContext.Provider>
   );
