eea111f04a6ab00788349d5715c9f29c548fa81b
nordinsimon <simon.s.nordin@gmail.com>
2023-11-09 23:59:55 +0100

    AddedCss

--- src/components/admin/InviteUser.tsx
+++ src/components/admin/InviteUser.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 
 //Content
-import "./admin.css";
+
 import { useAuth } from "../../context/AuthCtx";
 import api from "../../middleware/api";
 
@@ -10,6 +10,7 @@ const InviteUsers = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [invitationSent, setInvitationSent] = useState(false);
+  const [error, setError] = useState("");
   const { accessToken } = useAuth() as { accessToken: string };
 
   const handleLogin = async (e: React.FormEvent) => {
@@ -29,42 +30,52 @@ const InviteUsers = () => {
       });
       if (status === 200) {
         setInvitationSent(true);
+        setError("");
       } else {
         console.error("login failed");
       }
-    } catch (error) {
+      //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      const errorMessages = error.response.data.error;
+      setError(errorMessages);
       console.error(error, "Something went wrong");
     }
   };
 
   return (
     <div className="inviteUsersContainer">
-      <form onSubmit={handleLogin}>
-        <label>
-          Email:
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-              setInvitationSent(false);
-            }}
-          />
-        </label>
-        <label>
-          Name:
-          <input
-            type="name"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-              setInvitationSent(false);
-            }}
-          />
-        </label>
+      <h3>Invite New User To Newsfeed</h3>
+      <form className="inviteFormContainer" onSubmit={handleLogin}>
+        <div className="inviteForm">
+          <label>
+            Email:
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setInvitationSent(false);
+                setError("");
+              }}
+            />
+          </label>
+          <label>
+            Name:
+            <input
+              type="name"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+                setInvitationSent(false);
+                setError("");
+              }}
+            />
+          </label>
+        </div>
         <button type="submit">Invite</button>
       </form>
       {invitationSent && <p>Invitation sent</p>}
+      <p>{error}</p>
     </div>
   );
 };
